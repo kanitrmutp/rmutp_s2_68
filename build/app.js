a8747ec1cfa8c92ddc73009662d7c31a9a794084
Kanit <Kanit-P@rmutp.ac.th>
2025-08-31 15:21:42 +0700

    add tsconfig

--- build/app.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var hono_1 = require("hono");
-var app = new hono_1.Hono();
-app.get("/", function (c) { return c.text("Hello World"); });
-app.get("/profile", function (c) { return c.text("Profile"); });
-exports.default = app;
